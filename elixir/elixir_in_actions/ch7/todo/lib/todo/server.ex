3fd3e66ca2eff1b9706a5452d149d53ef574c006
chuanwen <chuanwen@gmail.com>
2016-08-18 23:06:30 -0700

    make server init light -- so Todo.Cache can handle more concurrent request.

--- elixir/elixir_in_actions/ch7/todo/lib/todo/server.ex
+++ elixir/elixir_in_actions/ch7/todo/lib/todo/server.ex
@@ -14,7 +14,8 @@ defmodule Todo.Server do
     end
 
     def init(name) do
-        {:ok, {name, Todo.Database.get(name) || Todo.List.new}}
+        send(self, {:real_init, name})
+        {:ok, nil}
     end
 
     def handle_cast({:add_entry, entry}, {name, todo_list}) do
@@ -26,4 +27,11 @@ defmodule Todo.Server do
     def handle_call({:entries, date}, _, {name, todo_list}) do
         {:reply, Todo.List.entries(todo_list, date), {name, todo_list}}
     end
+
+    def handle_info({:real_init, name}, _) do 
+        {:noreply, {name, Todo.Database.get(name) || Todo.List.new}}
+    end
+
+    def handle_info(_, state), do: {:noreply, state}
+    
 end
