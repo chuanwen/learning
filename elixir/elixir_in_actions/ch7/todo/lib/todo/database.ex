041b9b72c7c037f0314ce8357472a720a8aef288
chuanwen <chuanwen@gmail.com>
2016-08-20 12:02:24 -0700

    re-structure database: server + a pool of worker

--- elixir/elixir_in_actions/ch7/todo/lib/todo/database.ex
+++ elixir/elixir_in_actions/ch7/todo/lib/todo/database.ex
@@ -10,15 +10,61 @@ defmodule Todo.Database do
     end
 
     def store(key, data) do
-        GenServer.cast(:todo_database, {:store, key, data})
+        worker_pid = get_worker(key)
+        Todo.DatabaseWorker.store(worker_pid, key, data)
     end
 
     def get(key) do
-        GenServer.call(:todo_database, {:get, key})
+        worker_pid = get_worker(key)
+        Todo.DatabaseWorker.get(worker_pid, key)
     end
 
     def delete(key) do
-        GenServer.cast(:todo_database, {:delete, key})
+        worker_pid = get_worker(key)
+        Todo.DatabaseWorker.delete(worker_pid, key)
+    end
+
+    def get_worker(key) do
+        GenServer.call(:todo_database, {:get_worker, key})
+    end
+
+    def init(db_folder) do
+        workers = 0..2 |> Enum.into(%{},
+            fn(key) -> 
+                {:ok, pid} = Todo.DatabaseWorker.start(db_folder)
+                {key, pid}
+            end)
+        {:ok, workers}
+    end
+
+    def handle_call({:get_worker, key}, _, workers) do
+        index = :erlang.phash2(key, 3)
+        {:reply, workers[index], workers}
+    end
+
+end
+
+defmodule Todo.DatabaseWorker do
+    use GenServer
+
+    def start(db_folder) do
+        GenServer.start(__MODULE__, db_folder)
+    end
+
+    def stop(pid) do
+        GenServer.stop(pid)
+    end
+
+    def store(pid, key, data) do
+        GenServer.cast(pid, {:store, key, data})
+    end
+
+    def get(pid, key) do
+        GenServer.call(pid, {:get, key})
+    end
+
+    def delete(pid, key) do
+        GenServer.cast(pid, {:delete, key})
     end
 
     def init(db_folder) do
@@ -43,5 +89,4 @@ defmodule Todo.Database do
         end
         {:reply, data, db_folder}
     end
-
 end
