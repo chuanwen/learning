6a8141149428892d81cdf253caafe71e2fa0ded8
chuanwen <chuanwen@gmail.com>
2016-08-20 10:41:45 -0700

    bugfix of default selected

--- shiny/03_rank/ui.R
+++ shiny/03_rank/ui.R
@@ -6,7 +6,7 @@ shinyUI(fluidPage(
 
     # Sidebar with a slider input for the number of bins
     sidebarLayout(
-        sidebarPanel(selectInput("f1", "feature 1:", letters[c(1:10)], selected=1)),
+        sidebarPanel(selectInput("f1", "feature 1:", letters[c(1:10)], selected="a")),
         mainPanel(
             h3("Ordered similarity of feature 1 with other features"),
             tableOutput("f2")
